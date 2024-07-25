cecfd264d4041f800387fb80951149080d57a648
P SANGEETHA <130374298+sangeetha4597@users.noreply.github.com>
2024-07-25 22:30:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,108 +1,41 @@
-const express = require("express");
-const cors = require("cors");
-require("./db/config");
-const User = require('./db/User');
-const Product = require("./db/Product")
-const Jwt = require('jsonwebtoken');
-const jwtKey = 'e-com';
-const app = express();
-
-app.use(express.json());
-app.use(cors());
 
 
-//  register 
-app.post("/register", async (req, resp) => {
-    let user = new User(req.body);
-    let result = await user.save();
-    result = result.toObject();
-    delete result.password
-    Jwt.sign({result}, jwtKey, {expiresIn:"2h"},(err,token)=>{
-        if(err){
-            resp.send("Something went wrong")  
-        }
-        resp.send({result,auth:token})
-    })
-})
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
 
-app.post("/login", async (req, resp) => {
-    if (req.body.password && req.body.email) {
-        let user = await User.findOne(req.body).select("-password");
-        if (user) {
-            Jwt.sign({user}, jwtKey, {expiresIn:"2h"},(err,token)=>{
-                if(err){
-                    resp.send("Something went wrong")  
-                }
-                resp.send({user,auth:token})
-            })
-        } else {
-            resp.send({ result: "No User found here" })
-        }
-    } else {
-        resp.send({ result: "No User found.." })
-    }
-});
+        if (this.hash !== "") {
 
-app.post("/add-product", async (req, resp) => {
-    let product = new Product(req.body);
-    let result = await product.save();
-    resp.send(result);
-});
+            event.preventDefault();
 
-app.get("/products", async (req, resp) => {
-    const products = await Product.find();
-    if (products.length > 0) {
-        resp.send(products)
-    } else {
-        resp.send({ result: "No Product found" })
-    }
-});
+            var hash = this.hash;
 
-app.delete("/product/:id", async (req, resp) => {
-    let result = await Product.deleteOne({ _id: req.params.id });
-    resp.send(result)
-}),
-
-    app.get("/product/:id", async (req, resp) => {
-        let result = await Product.findOne({ _id: req.params.id })
-        if (result) {
-            resp.send(result)
-        } else {
-            resp.send({ "result": "No Record Found." })
-        }
-    })
-
-app.put("/product/:id", async (req, resp) => {
-    let result = await Product.updateOne(
-        { _id: req.params.id },
-        { $set: req.body }
-    )
-    resp.send(result)
-});
-
-app.put("/product/:id", async (req, resp) => {
-    let result = await Product.updateOne(
-        { _id: req.params.id },
-        { $set: req.body }
-    )
-    resp.send(result)
-});
-
-app.get("/search/:key", async (req, resp) => {
-    let result = await Product.find({
-        "$or": [
-            {
-                name: { $regex: req.params.key }  
-            },
-            {
-                company: { $regex: req.params.key }
-            },
-            {
-                category: { $regex: req.params.key }
-            }
-        ]
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
     });
-    resp.send(result);
-})
+});
 
-app.listen(5000);
+// navbar toggle
+
+// $('#nav-toggle').click(function(){
+//     $(this).toggleClass('is-active')
+//     $('ul.nav').toggleClass('show');
+// });
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const toggler = document.querySelector(".navbar-toggler");
+    const menu = document.querySelector(".navbar-menu");
+    const icon = document.querySelector(".navbar-toggler-icon");
+
+    toggler.addEventListener("click", function() {
+        menu.classList.toggle("active");
+        icon.classList.toggle("active");
+    });
+});
